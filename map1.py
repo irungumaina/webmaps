7033dc3392ad87dd1f4b9d7c0e8ece1288b57698
irungumaina <kirathimo2010@gmail.com>
2019-05-01 15:28:58 +0300

    added color to the markers

--- map1.py
+++ map1.py
@@ -4,15 +4,25 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 
 map = folium.Map(location=[38.58, -99.09],
-                 zoom_start=6, tiles="Mapbox Bright")
+                 zoom_start=5, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(
-        location=[lt, ln], popup="This is the Marker", icon=folium.Icon(color='green')))
+        location=[lt, ln], popup=str(el), icon=folium.Icon(color=color_producer(el))))
 map.add_child(fg)
 
 map.save("Map1.html")

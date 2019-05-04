500a7e7b1f86bf3990d25cac4e204dd086c390f0
irungumaina <kirathimo2010@gmail.com>
2019-05-04 10:53:48 +0300

    added another layer and polygons on the map

--- map1.py
+++ map1.py
@@ -22,7 +22,11 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(
-        location=[lt, ln], radius=6, popup=str(el)+"m", fill_color=color_producer(el), color='grey', fill_opacity=(0.7)))
-map.add_child(fg)
+        location=[lt, ln], radius=6, popup=str(el)+"m",
+        fill_color=color_producer(el), color='grey', fill_opacity=(0.7)))
 
+fg.add_child(folium.GeoJson(
+    data=(open('world.json', 'r', encoding='UTF-8').read())))
+
+map.add_child(fg)
 map.save("Map1.html")
